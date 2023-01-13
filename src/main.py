390859b8b57ae380ca8f0dd21cfd62d7bd1c8c8d
lemmgua <alejandrozubirifunes@gmail.com>
2023-01-13 12:52:16 +0100

    dsa

--- src/main.py
+++ src/main.py
@@ -8,6 +8,9 @@ split = ""
 if (info["vocales"] == info["consonantes"]):
     split = [palabra[i:i+2] for i in range(0, len(palabra), 2)]
 
-for i in split:
+for i in split.reverse():
     if (len(re.findall("[Á-ú]", i) )> 0):
-        
+        if i > 3:
+            print(acentos[3])
+        else:
+            print(acentos[i])
