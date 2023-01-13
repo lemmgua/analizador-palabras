061b8a4a45f5606a77e5f4dbfa30cba373c7e6d3
lemmgua <alejandrozubirifunes@gmail.com>
2023-01-13 12:27:08 +0100

    dsa

--- Analizadordepalabras.py
+++ Analizadordepalabras.py
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:Analizadordepalabras.py
 from time import *
 import os
 
@@ -10,48 +9,9 @@ hacer un For Loop por las letras y comparar si son vocales'''
 
 '''Forma muyyy simple
 import pyphen
-b = input("Dime una palabra:\n")
+b = input("Dime una palabra:\palabra")
 a = pyphen.Pyphen(lang='ca')
 print(a.inserted(b))'''
-
-continueSelect = 1
-
-while continueSelect == 1:
-    
-    sleep(0.3)
-    n = input("Dime una palabra:\n")
-    nSplitList = []
-
-    vocales = "aeiou"
-
-    def isVocal(letter):
-        return (letter in vocales)
-        
-
-    for i in range(0, len(n)):
-        if i+2 < len(n) and isVocal(n[i+1]) == False and isVocal(n[i+2]) == False:
-            nSplitList.append(n[i-1:i+2])
-        elif i+1 != len(n) and n[-1] == n[i+1] and isVocal(n[-1]) == False:
-            nSplitList.append(n[i-1:])
-        elif n[i] == "í":
-            nSplitList.append(n[i])
-        elif isVocal(n[i]):
-            nSplitList.append(n[i-1:i+1])       
-    print(" - ".join(nSplitList))
-    sleep(1)
-
-    continueSelect = int(input("¿Desea continuar?\nSi: 1\nNo: 0\n"))
-    if(continueSelect == 0):
-        sleep(0.3)
-        print("Finalizado")
-        break
-    if continueSelect == 1 or continueSelect == 0:
-        continue
-    else:
-        while continueSelect != 1 or continueSelect != 0:
-            continueSelect = int(input("El valor introducido no es válido. Introduce otro\nSi: 1\nNo: 0\n"))
-            if continueSelect is 1 or continueSelect is 0:
-                break
 
 def silabas(palabra):
     nSplitList = []
@@ -61,15 +21,15 @@ def silabas(palabra):
     def isVocal(letter):
         return (letter in vocales)
     
-    for i in range(0, len(n)):
-        if i+2 < len(n) and isVocal(n[i+1]) == False and isVocal(n[i+2]) == False:
-            nSplitList.append(n[i-1:i+2])
-        elif i+1 != len(n) and n[-1] == n[i+1] and isVocal(n[-1]) == False:
-            nSplitList.append(n[i-1:])
-        elif n[i] == "í":
-            nSplitList.append(n[i])
-        elif isVocal(n[i]):
-            nSplitList.append(n[i-1:i+1]) 
+    for i in range(0, len(palabra)):
+        if i+2 < len(palabra) and isVocal(palabra[i+1]) == False and isVocal(palabra[i+2]) == False:
+            nSplitList.append(palabra[i-1:i+2])
+        elif i+1 != len(palabra) and palabra[-1] == palabra[i+1] and isVocal(palabra[-1]) == False:
+            nSplitList.append(palabra[i-1:])
+        elif palabra[i] == "í":
+            nSplitList.append(palabra[i])
+        elif isVocal(palabra[i]):
+            nSplitList.append(palabra[i-1:i+1]) 
     
     return nSplitList
     
@@ -92,7 +52,7 @@ print(a.inserted(b))
 
 while continueSelect is 1:
     sleep(0.3)
-    n = input("Dime una palabra:\n")
+    palabra = input("Dime una palabra:\palabra")
     nSplitList = []
 
     vocales = "aeiou"
@@ -101,16 +61,16 @@ while continueSelect is 1:
         return (letter in vocales)
         
 
-    for i in range(0, len(n)):
-        if i+2 < len(n) and isVocal(n[i+1]) == False and isVocal(n[i+2]) == False:
-            nSplitList.append(n[i-1:i+2])
-        elif i+1 != len(n) and n[-1] == n[i+1] and isVocal(n[-1]) == False:
-            nSplitList.append(n[i-1:])
-        elif isVocal(n[i]):
-            nSplitList.append(n[i-1:i+1])
+    for i in range(0, len(palabra)):
+        if i+2 < len(palabra) and isVocal(palabra[i+1]) == False and isVocal(palabra[i+2]) == False:
+            nSplitList.append(palabra[i-1:i+2])
+        elif i+1 != len(palabra) and palabra[-1] == palabra[i+1] and isVocal(palabra[-1]) == False:
+            nSplitList.append(palabra[i-1:])
+        elif isVocal(palabra[i]):
+            nSplitList.append(palabra[i-1:i+1])
     print(" - ".join(nSplitList))
     sleep(1)
-    continueSelect = int(input("¿Desea continuar?\nSi: 1\nNo: 0\n"))
+    continueSelect = int(input("¿Desea continuar?\nSi: 1\nNo: 0\palabra"))
     if(continueSelect == 0):
         sleep(0.3)
         print("Finalizado")
@@ -119,7 +79,7 @@ while continueSelect is 1:
         continue
     else:
         while continueSelect != 1 or continueSelect != 0:
-            continueSelect = int(input("El valor introducido no es válido. Introduce otro\nSi: 1\nNo: 0\n"))
+            continueSelect = int(input("El valor introducido no es válido. Introduce otro\nSi: 1\nNo: 0\palabra"))
             if continueSelect is 1 or continueSelect is 0:
                 break'''
 >>>>>>> Stashed changes:Analizador de palabras.py
