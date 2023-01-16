7d6b865b35808477a43b3617e76b9a20dde1d7ee
lemmgua <alejandrozubirifunes@gmail.com>
2023-01-16 10:25:01 +0100

    dsa

--- src/Sílabas.py
+++ src/Sílabas.py
@@ -1,8 +1,12 @@
 import re
 
 def silabas(palabra):
+    consonantes = "[b-df-hj-np-tv-xz]"
+    vocales = "[aeiou]"
     #consonante o unidad consonántica seguida de una vocal
     #ll, ny, gu y qu seguida de vocal es una sola sílaba
 
     #utilizar regex para detectar las reglas
-    pass
+    return re.findall(".*?[b-df-hj-np-tv-xz]{1,2}[aeiou]", palabra)
+
+print(silabas(input("Dame una palabra\n")))
