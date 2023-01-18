a8b7f72330f60471a69206b9c5a224a39733fedc
lemmgua <alejandrozubirifunes@gmail.com>
2023-01-18 09:18:02 +0100

    ds

--- src/Sílabas.py
+++ src/Sílabas.py
@@ -1,15 +1,21 @@
 import re
 
 def silabas(palabra):
-    consonantes = "[b-df-hj-np-tv-xz]"
-    vocales = "[aeiou]"
+    digrafs = ("rr", "ss", "sc", "ix", "tl", "tll", "tj", "tg", "tm", "tn", "tx")
+    silabas = []
     
     #consonante o unidad consonántica seguida de una vocal
     #ll, ny, gu y qu seguida de vocal es una sola sílaba
 
     #utilizar regex para detectar las reglas
     if (len(re.findall(".*?[b-df-hj-np-tv-xz]{1}[aeiou]", palabra)) > 0):
-        return re.findall(".*?[b-df-hj-np-tv-xz]{1}[aeiou]", palabra)
-    return re.findall(".*?[b-df-hj-np-tv-xz]{1}[aeiou]|[b-df-hj-np-tv-xz]{1,2}[aeiou]", palabra)
+        silabas = re.findall(".*?[b-df-hj-np-tv-xz]{1}[aeiou]", palabra)
+    silabas = re.findall(".*?[b-df-hj-np-tv-xz]{1}[aeiou]|[b-df-hj-np-tv-xz]{1,2}[aeiou]", palabra)
+    for i in range(len(silabas)):
+        for j in digrafs:
+            if silabas[i].startswith(j):
+                silabas[i-1] += silabas[i][0]
+                silabas[i] = silabas[i][1:]
+    return silabas
 
 print(" - ".join(silabas(input("Dame una palabra\n"))))
