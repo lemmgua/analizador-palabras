32b2830ee95071c93aa1220437eac2e395362d9b
lemmgua <alejandrozubirifunes@gmail.com>
2023-02-02 23:26:58 +0100

    re.search("[b-df-hj-np-tv-xz]{1}[aeiou]{1}[b-df-hj-np-tv-xz]{1}", palabra)

---
+++ src/Test/research.py
@@ -0,0 +1,22 @@
+import re
+
+def silabas(palabraAAnalizar):
+    palabra = palabraAAnalizar
+    silabas = []
+    busqueda = None
+
+    while (len(palabra) > 0):
+        if (re.search("[aeiou]{1}[b-df-hj-np-tv-xz]{2}[aeiou]{1}", palabra) != None):
+            busqueda = re.search("[aeiou]{1}[b-df-hj-np-tv-xz]{1}", palabra)
+        elif (re.search("[b-df-hj-np-tv-xz]{2}[aeiou]{1}[b-df-hj-np-tv-xz]{1}", palabra) != None):
+            busqueda = re.search("[b-df-hj-np-tv-xz]{2}[aeiou]{1}", palabra)
+        elif (re.search("[b-df-hj-np-tv-xz]{1}[aeiou]{1}[b-df-hj-np-tv-xz]{1}", palabra) != None):
+            busqueda = re.search("[b-df-hj-np-tv-xz]{1}[aeiou]{1}[b-df-hj-np-tv-xz]{1}", palabra)
+        silabas.append(palabra[busqueda.start():busqueda.end()])
+        palabra = palabra[busqueda.end():]
+    
+    return silabas
+
+
+if __name__ == "__main__":
+    print(silabas(input("carlos los taxis:\n")))
